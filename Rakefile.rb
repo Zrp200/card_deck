a1cb78d8824362c6a3b0759a1dbb78134c18f65d
Zachary Perlmutter <zrp200@gmail.com>
2014-09-08 18:15:32 -0700

    Update Rakefile.rb

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,11 +1,12 @@
-def build(version)
-	`git pull`
-	`gem build card_deck.gemspec`
-	`gem install card_deck-#{version}.gem`
-	`git add card_deck-#{version}.gem && git commit card_deck-#{version}.gem -m "rake build"`
-	`git push`
-end
+
 namespace 'build' do
+	def build(version)
+		`git pull`
+		`gem build card_deck.gemspec`
+		`gem install card_deck-#{version}.gem`
+		`git add card_deck-#{version}.gem && git commit card_deck-#{version}.gem -m "rake build"`
+		`git push`
+	end
 	task '000'.to_sym {build "0.0.0"}
 	task '010'.to_sym {build "0.1.0"}
 	task '011'.to_sym {build "0.1.1"}
