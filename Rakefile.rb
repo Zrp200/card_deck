a82a34c7ba8eb0ceafe508a06e4fd8748bfabdc1
Zachary Perlmutter <zrp200@gmail.com>
2014-09-08 17:15:14 -0700

    Update

--- Rakefile.rb
+++ Rakefile.rb
@@ -8,7 +8,7 @@ namespace 'build' do
 	task '010'.to_sym do
 		`git pull`
 		`gem build card_deck.gemspec`
-		`git add card_deck-0.1.0.gem && git commit card-0.1.0 -m "rake build:010"`
+		`git add card_deck-0.1.0.gem && git commit card_deck-0.1.0.gem -m "rake build:010"`
 		`git push`
 	end
 end
