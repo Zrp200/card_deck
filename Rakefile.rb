6d3e5dcdf5a4ffd2576cceb2d37b3f474d105ecf
Zachary Perlmutter <zrp200@gmail.com>
2014-09-08 17:04:02 -0700

    Update and rename Rakefile to Rakefile.rb

--- Rakefile.rb
+++ Rakefile.rb
@@ -5,4 +5,10 @@ namespace 'build' do
 		`git commit card_deck-0.0.0.gem -m Built_0.0.0_gem`
 		`git push`
 	end
+	task '010'.to_sym do
+		`git pull`
+		`gem build card_deck.gemspec`
+		`git add card_deck-0.1.0.gem && git commit card-0.1.0 -m "rake build:010"`
+		`git push`
+	end
 end
