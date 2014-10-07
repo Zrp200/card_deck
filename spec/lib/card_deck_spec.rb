166bc8cd270bfca8030bcdc68cca310e3221d109
Zachary Perlmutter <zrp200@gmail.com>
2014-10-06 17:47:05 -0700

    :clubs:

--- spec/lib/card_deck_spec.rb
+++ spec/lib/card_deck_spec.rb
@@ -7,13 +7,19 @@ describe Card do
 			expect(subject).to eq "\u2660"
 		end
 	end
-    describe "#abbr" do
-	context "@suit = SPADES and 3" do
-		subject {Card.new 3, Card::SPADES}
-		it "should return #{Card::SPADES}3" do
-			expect(subject.abbr).to eq "#{Card::SPADES}3"
+	describe "::CLUBS" do
+		subject {Card::CLUBS}
+		it "should equal \u2663" do
+			expect(subject).to eq "\u2663"
 		end
 	end
+    describe "#abbr" do
+		context "@suit = SPADES and 3" do
+			subject {Card.new 3, Card::SPADES}
+			it "should return #{Card::SPADES}3" do
+				expect(subject.abbr).to eq "#{Card::SPADES}3"
+			end
+		end
     end
 end
     
