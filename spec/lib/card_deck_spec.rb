6e0c777bb0137431383e8cd4de842ba6eef34eff
Zachary Perlmutter <zrp200@gmail.com>
2014-10-06 17:47:57 -0700

    Update card_deck_spec.rb

--- spec/lib/card_deck_spec.rb
+++ spec/lib/card_deck_spec.rb
@@ -14,7 +14,7 @@ describe Card do
 		end
 	end
     describe "#abbr" do
-		context "@suit = SPADES and 3" do
+		context "@suit, @num = SPADES, 3" do
 			subject {Card.new 3, Card::SPADES}
 			it "should return #{Card::SPADES}3" do
 				expect(subject.abbr).to eq "#{Card::SPADES}3"
