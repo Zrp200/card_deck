f982fcee42bf1849a19b79f75041303fdb7a4ce7
Zachary Perlmutter <zrp200@gmail.com>
2014-10-06 17:36:57 -0700

    Update card_deck_spec.rb

--- spec/lib/card_deck_spec.rb
+++ spec/lib/card_deck_spec.rb
@@ -1,6 +1,10 @@
 require 'card_deck'
 include CardDeck
 describe Card do
+	describe "::SPADES" do
+		subject {Card::SPADES}
+		it "should equal \u2660" {expect(subject).to eq "\u2660"
+	end
     describe "#abbr" do
 	context "@suit = SPADES and 3" do
 		subject {Card.new 3, Card::SPADES}
