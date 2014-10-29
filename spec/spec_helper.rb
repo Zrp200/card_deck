b118a096fe1675c4173291650490c16436a4299c
Zachary Perlmutter <zrp200@gmail.com>
2014-10-28 17:04:14 -0700

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,7 @@
 # The `.rspec` file also contains a few flags that are not defaults but that
 # users commonly want.
 require "card_deck"
+require "rspec/its"
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
 # The settings below are suggested to provide a good initial experience
