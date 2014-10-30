f349a8c931d7f8bfcc1f8a4138b31e9babaf223e
Zachary Perlmutter <zrp200@gmail.com>
2014-10-30 15:53:22 -0700

    Update card_deck_spec.rb

--- spec/lib/card_deck_spec.rb
+++ spec/lib/card_deck_spec.rb
@@ -2,10 +2,9 @@ include CardDeck
 RSpec.describe Card do
 	describe "#new" do
 		subject {Card.gen}
-		its(:inspect) {is_expected.to eq(subject.abbr)}
-		its(:abbr) {is_expected.to eq(subject.inspect)}
+		it {is_expected.to respond_to(:num, :suit, :to_s, :inspect, :abbr, :abbreviation)}
+		its(:abbr) {is_expected.to eq(subject.subject.abbreviation)}
 		its(:to_s) {is_expected.to eq(subject.inspect)}
-		it {is_expected.to respond_to(:num, :suit)}
 	end
 	describe "::SPADES" do
 		subject {Card::SPADES}
@@ -13,9 +12,22 @@ RSpec.describe Card do
 	end
 	describe "::CLUBS" do
 		subject {Card::CLUBS}
-		it "should equal \u2663" do
-			expect(subject).to eq "\u2663"
-		end
+		it {is_expected.to eq("\u2663")}
+	end
+	describe "::HEARTS" do
+		subject {Card::HEARTS}
+		it {is_expected.to eq("\u2665")
+	end
+	describe "::DIAMONDS" do
+		subject {Card::DIAMONDS}
+		it {is_expected.to eq("\u2666")}
+	end
+end
+RSpec.describe Deck do
+	describe "#new" do
+		subject {Deck.new}
+		it {is_expected.to respond_to(:cards, :inspect)}
+		its(:cards) {is_expected.to eq(subject.inspect)}
 	end
 end
     
