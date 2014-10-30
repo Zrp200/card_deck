1e4caf1ba698eea4b7e600f1ff93caf001ade47e
Zachary Perlmutter <zrp200@gmail.com>
2014-10-29 20:58:13 -0700

    Update card_deck_spec.rb

--- spec/lib/card_deck_spec.rb
+++ spec/lib/card_deck_spec.rb
@@ -1,6 +1,5 @@
 include CardDeck
 RSpec.describe Card do
-	it {is_expected.to respond_to(:gen)}
 	describe "#new" do
 		subject {Card.gen}
 		its(:inspect) {is_expected.to eq(subject.abbr)}
@@ -8,43 +7,12 @@ RSpec.describe Card do
 	end
 	describe "::SPADES" do
 		subject {Card::SPADES}
-		it "is expected to == \u2660" do 
-			expect(subject).to eq("\u2660")
-		end
+		it {is_expected.to eq("\u2660")}
 	end
 	describe "::CLUBS" do
 		subject {Card::CLUBS}
 		it "should equal \u2663" do
 			expect(subject).to eq "\u2663"
-		end
-	end
-	describe "#abbr" do
-		for suit in Card::SUIT
-			for num in (Card::NUM - ["Joker"])
-				context "@num, @suit = #{num}, #{suit}" do
-					subject = Card.new num, suit
-					case String(subject.num).length
-						when 1
-							it "should return #{subject.suit}#{subject.num}" do
-								expect(subject.abbr).to eq "#{subject.suit}#{subject.num}"
-							end
-						when 2
-							it "should return #{subject.suit}#{subject.num}" do
-								expect(subject.abbr).to eq "#{subject.suit}#{subject.num}"
-							end
-					else
-						it "should return #{subject.suit}#{(subject.num)[0]}" do
-							expect(subject.abbr).to eq "#{subject.suit}#{(subject.num.to_s)[0]}"
-						end
-					end
-				end
-			end
-		end
-		context "@num = \"Joker\"" do
-			subject = Card.new "Joker"
-			it "should return \"Joker\"" do
-				expect(subject.abbr).to eq "#{subject.num}"
-			end
 		end
 	end
 end
