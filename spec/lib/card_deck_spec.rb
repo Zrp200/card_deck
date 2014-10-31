84911591a5c07fa739a64b98f8ea78271741eacd
Zachary Perlmutter <zrp200@gmail.com>
2014-10-30 18:32:25 -0700

    Update card_deck_spec.rb

--- spec/lib/card_deck_spec.rb
+++ spec/lib/card_deck_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe Card do
 	describe "#new" do
 		subject {Card.gen}
 		it {is_expected.to respond_to(:num, :suit, :to_s, :inspect, :abbr, :abbreviation)}
-		its(:abbr) {is_expected.to eq(subject.subject.abbreviation)}
+		its(:abbr) {is_expected.to eq(subject.abbreviation)}
 		its(:to_s) {is_expected.to eq(subject.inspect)}
 	end
 	describe "::SPADES" do
