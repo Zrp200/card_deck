1d9c700cb40043f4237aa922c2face0d9b363847
Zachary Perlmutter <zrp200@gmail.com>
2014-10-31 17:14:12 -0700

    Update and rename card_deck_spec.rb to card_spec.rb
    [skip ci]

--- spec/lib/card_spec.rb
+++ spec/lib/card_spec.rb
@@ -34,18 +34,5 @@ RSpec.describe Card do
 		it {is_expected.to include Card::DIAMONDS, Card::HEARTS, Card::SPADES, Card::CLUBS}
 	end
 end
-RSpec.describe Deck do
-	describe "#new" do
-		subject {Deck.new}
-		it {is_expected.to respond_to(:cards, :inspect)}
-		its(:cards) {is_expected.to eq(subject.inspect)}
-		context ":jokers == true" do
-			subject {Deck.new jokers: true}
-			describe subject.cards do
-				its(:length) {is_expected.to eq 54}
-			end
-		end
-	end
-end
 
     
