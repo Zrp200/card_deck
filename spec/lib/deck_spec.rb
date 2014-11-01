633346d827638614a5ccb77886086fc16bd2a03d
Zachary Perlmutter <zrp200@gmail.com>
2014-10-31 17:24:56 -0700

    Update deck_spec.rb

--- spec/lib/deck_spec.rb
+++ spec/lib/deck_spec.rb
@@ -1,13 +1,22 @@
+require "rspec/expectations"
+RSpec::Matchers.define :have_jokers do
+	match do |actual|
+		jokers = false
+		for card in actual
+			jokers = true if card.num == "Joker"
+		end
+		return jokers
+	end
+end
 RSpec.describe Deck do
 	describe "#new" do
 		subject {Deck.new}
 		it {is_expected.to respond_to(:cards, :inspect)}
 		its(:cards) {is_expected.to eq(subject.inspect)}
+		its(:cards) {is_expected.to_not have_jokers}
 		context ":jokers == true" do
 			subject {Deck.new jokers: true}
-			describe subject.cards do
-				its(:length) {is_expected.to eq 54}
-			end
+			its(:cards) {is_expected.to have_jokers}
 		end
 	end
 end
