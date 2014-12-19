f19b9b9456692e7e4fa1af99657e21df56ffb294
Zachary Perlmutter <zrp200@gmail.com>
2014-12-19 11:50:57 -0800

    Update card.rb

--- lib/card.rb
+++ lib/card.rb
@@ -1,5 +1,10 @@
 module CardDeck # The gem
-	def Card(num=Card::Num, suit=Card::Suit.sample); Card.new num, suit; end # an easier way to generate a card
+
+=begin
+An easier way to generate a card
+@see (Card#new)
+=end
+	def Card(num=Card::Num.sample, suit=Card::Suit.sample); Card.new num, suit; end
 	class CardError < StandardError; end # Errors for when you incorrectly use a card.
 	class Card # The central part of any card game. It is what makes card games 'Card' games.
 		Hearts, Spades, Diamonds, Clubs = "\u2665", "\u2660", "\u2666", "\u2663" # Suits
@@ -7,7 +12,13 @@ module CardDeck # The gem
 		Suit = [Hearts, Spades, Diamonds, Clubs] # Legal arguments for parameter suit in Card#new
 		attr_accessor :num # The card's number. Must be Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King, or Joker
 		attr_accessor :suit # The card's suit. Must be Spades, Diamonds, Clubs, or Hearts.
-		def initialize(num=Num.sample, suit=Suit.sample) # Creates a new card. Parameter num is the card's number. Parameter suit is the card's suit. When given no arguments, the num and suit are randomly generated
+
+=begin
+ Creates a new card. Parameter num is the card's number.
+ @param suit [String] 
+ @param num [String, Fixnum]
+=end
+		def initialize(num=Num.sample, suit=Suit.sample)
 			unless Suit.include? suit
 				suit = case suit.downcase
 					when "diamonds" then Diamonds
@@ -18,7 +29,12 @@ module CardDeck # The gem
 			end
 			@num, @suit = num, suit
 		end
-		def abbreviation # The shorter representation of the card
+
+=begin
+The shorter representation of the card
+@return [String]
+=end
+		def abbreviation
 			unless @num == "Joker"
 				if @num == 10 then "#{@suit}#{@num}"
 				else
@@ -28,8 +44,8 @@ module CardDeck # The gem
 				@num
 			end
 		end
-		def black?; suit == Spades || suit == Clubs; end # Tests if the suit color is black
-		def red?; suit == Hearts || suit == Diamonds; end # Tests if the suit color is red
+		def black?; suit == Spades | Clubs; end # @return [Boolean]
+		def red?; suit == Hearts | Diamonds; end # @return [Boolean]
 		alias abbr abbreviation # A shorter method name
 		alias to_s abbr
 		alias inspect abbreviation
